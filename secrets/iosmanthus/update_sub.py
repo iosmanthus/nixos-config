05d9bd4ae5947db7341db97ff38f60019fae025f
iosmanthus <myosmanthustree@gmail.com>
2022-05-09 15:35:15 +0800

    bump clash config
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- secrets/iosmanthus/update_sub.py
+++ secrets/iosmanthus/update_sub.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python3 -p sops python3 python3Packages.pyyaml python3Packages.requests
+#! nix-shell -i python3 -p sops python3 python3Packages.pyyaml python3Packages.requests -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz
 
 import os
 import sys
