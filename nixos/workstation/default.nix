ad2df6fc221000d0f08be130d7308da7b381e2e9
iosmanthus <myosmanthustree@gmail.com>
2024-01-26 13:22:15 +0800

    remove global ldso
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- nixos/workstation/default.nix
+++ nixos/workstation/default.nix
@@ -21,11 +21,6 @@
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     stateVersion = "20.09";
-    # activationScripts.ldso = lib.stringAfter [ "usrbinenv" ] ''
-    #   mkdir -m 0755 -p /lib64
-    #   ln -sfn ${pkgs.glibc.out}/lib64/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2.tmp
-    #   mv -f /lib64/ld-linux-x86-64.so.2.tmp /lib64/ld-linux-x86-64.so.2 # atomically replace
-    # '';
   };
 
   nix = {
